562627869f9dd4f9055cc840b48e53ebe223281b
busterbrook <espzorlac@gmail.com>
2023-06-26 13:31:46 +0300

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Саня Якунин on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
